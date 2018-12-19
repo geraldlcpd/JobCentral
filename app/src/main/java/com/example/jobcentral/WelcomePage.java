1cde32bf31f216efeb61891b41293673a469d262
Gerald <geraldlcpd@gmail.com>
2018-12-19 14:32:11 +0800

    added firebase RTDB connection

--- app/src/main/java/com/example/jobcentral/WelcomePage.java
+++ app/src/main/java/com/example/jobcentral/WelcomePage.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WelcomePage extends AppCompatActivity {
 
 
     Button btnLogin, btnJobseeker, btnRecruiter;
     Intent moveToLogin, moveToJobseeker, moveToRecruiter;
 
+    FirebaseDatabase dbJobCentral = FirebaseDatabase.getInstance();
+    DatabaseReference db1 = dbJobCentral.getReference("message");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
 
+        db1.setValue("Hello World!");
 
         btnJobseeker = findViewById(R.id.btnJobseeker);
         btnRecruiter = findViewById(R.id.btnRecruiter);
